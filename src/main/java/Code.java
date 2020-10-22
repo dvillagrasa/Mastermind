6d81b1884d1eb5e48bba970e6b455eb4eb3ae61b
Sergi <sergi.luque@e-campus.uab.cat>
2020-10-22 13:17:24 +0200

    First commit

---
+++ src/main/java/Code.java
@@ -0,0 +1,12 @@
+public class Code {
+    private final byte[] code ;
+
+    public Code(byte[] code) {
+        this.code = code;
+    }
+
+    public byte[] getCode() {
+        return this.code;
+    }
+}
+
