92e690b58a48a4cf19391e1ec99013205c45fd4b
Sergi <sergi.luque@e-campus.uab.cat>
2020-11-08 18:59:40 +0100

    test: :white_check_mark: Test: it should throw Runtime Exception when code is null

--- src/test/java/CodeTest.java
+++ src/test/java/CodeTest.java
@@ -1,18 +1,32 @@
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 
 public class CodeTest {
-  Code code=new Code(new byte[]{1,2,3,4,5});
+
+
+
   @Test
   public void testGetCode(){
     byte[] codeReal={1,2,3,4,5};
-    Assert.assertArrayEquals(codeReal,code.getCode());
+    Code code = null;
+    code = new Code(codeReal);
+    Assertions.assertArrayEquals(codeReal,code.getCode());
   }
 
   @Test
   public void testGetLength(){
-    Assert.assertEquals(code.getLenght(),5);
+    byte[] codeReal={1,2,3,4,5};
+    Code code = null;
+    code = new Code(codeReal);
+    Assertions.assertEquals(code.getLenght(),5);
   }
+
+  @Test
+  public void it_should_throw_RuntimeException_when_code_is_invalid() {
+    Assertions.assertThrows(RuntimeException.class,() -> new Code(null));
+  }
+
 
 }
