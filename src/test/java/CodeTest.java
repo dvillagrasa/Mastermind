fa85f307fa64e00cd88c9c0b1b5f83cef17a2e00
Sergi Luque <sergi.luque@e-campus.uab.cat>
2020-11-08 23:04:17 +0100

    test: :white_check_mark: it_should_throw_RuntimeException_when_code_has_invalid

--- src/test/java/CodeTest.java
+++ src/test/java/CodeTest.java
@@ -9,23 +9,46 @@ public class CodeTest {
 
   @Test
   public void testGetCode(){
-    byte[] codeReal={1,2,3,4,5};
-    Code code = null;
-    code = new Code(codeReal);
-    Assertions.assertArrayEquals(codeReal,code.getCode());
+    byte[][] validCodes= {
+            {1,2,3,4,5},
+            {0},
+            {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+    };
+    for (byte[] validCode : validCodes){
+      Code code = new Code(validCode);
+      Assertions.assertArrayEquals(validCode,code.getCode());
+    }
+
   }
 
   @Test
   public void testGetLength(){
     byte[] codeReal={1,2,3,4,5};
-    Code code = null;
-    code = new Code(codeReal);
+    Code code = new Code(codeReal);
     Assertions.assertEquals(code.getLenght(),5);
   }
 
   @Test
-  public void it_should_throw_RuntimeException_when_code_is_invalid() {
+  public void it_should_throw_RuntimeException_when_code_has_invalid() {
+    //The code has a null array
     Assertions.assertThrows(RuntimeException.class,() -> new Code(null));
+
+    //The code has a negative number
+    byte[] negativeColorArray = {8,-1};
+    Assertions.assertThrows(RuntimeException.class,() -> new Code(negativeColorArray));
+
+    //The code has a character
+    byte[][] charInColorArrays = {
+            {'r'},
+            {8,'a'},
+            {'4',0},
+    };
+    for (byte[] charInColorArray : charInColorArrays)
+      Assertions.assertThrows(RuntimeException.class,() -> new Code(charInColorArray));
+
+    //The code has a number greater than 127
+    byte[] bigColorArray = {8,127}; //no me deja el IDE poner un 128 a un byte
+    Assertions.assertThrows(RuntimeException.class,() -> new Code(bigColorArray));
   }
 
 
