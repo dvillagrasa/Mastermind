0678da95a68030f5c85410a0af2e2766b078f2ce
Sergi Luque <sergi.luque@e-campus.uab.cat>
2020-11-09 01:29:56 +0100

    test: :white_check_mark: Create Board Test and Add testBoard and testInvalidBoard

---
+++ src/main/java/Core/Board.java
@@ -0,0 +1,13 @@
+package Core;
+
+public class Board {
+    byte numberGuesses;
+
+    public Board(Code secretCode, byte numberGuesses) {
+
+    }
+
+    public byte getNumberGuesses() {
+        return numberGuesses;
+    }
+}
---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,30 @@
+import Core.Board;
+import Core.Code;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+public class BoardTest {
+    Code secretCode;
+
+    @BeforeEach
+    void setUp() {
+      secretCode = new Code(new byte[]{1, 2, 3, 4});
+    }
+
+    @Test
+    void testBoard() {
+        Board board = new Board(secretCode, (byte) 1);
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void testInvalidBoard() {
+        Assertions.assertThrows(RuntimeException.class,() -> new Board(null, (byte) 1));
+        Assertions.assertThrows(RuntimeException.class,() -> new Board(secretCode, (byte) 0));
+        Assertions.assertThrows(RuntimeException.class,() -> new Board(secretCode, (byte) -1));
+        Assertions.assertThrows(RuntimeException.class,() -> new Board(secretCode, (byte) 200));
+    }
+}
