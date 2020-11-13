6beb935792176b5530e92dae42b3617ee1e2b0c8
BernatMallol <bernatmallol@gmail.com>
2020-11-13 20:48:19 +0100

    feat: created Keys contructor and getLength function from Keys class.

--- src/main/java/Core/Keys.java
+++ src/main/java/Core/Keys.java
@@ -4,11 +4,13 @@ public class Keys {
   KeyPeg[] key;
 
   public Keys(KeyPeg[] key) {
+    if(key.length==0) throw new RuntimeException();
+    if(key==null) throw new RuntimeException();
     this.key = key;
 
   }
 
   public int getLength(){
-    return -1;
+    return key.length;
   }
 }
