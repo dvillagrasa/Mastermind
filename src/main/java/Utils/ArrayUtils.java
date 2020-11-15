d94f596edcdde7e41938a72fa7d89ef4e97479e1
BernatMallol <bernatmallol@gmail.com>
2020-11-15 17:58:16 +0100

    feat: reimplemented getRandomIntArray.

--- src/main/java/Utils/ArrayUtils.java
+++ src/main/java/Utils/ArrayUtils.java
@@ -8,7 +8,7 @@ public class ArrayUtils {
     public static int[] getRandomIntArray(int numberOfColors, int lengthCode){
         Utils.Random random = new Utils.Random();
         int[] code = new int[lengthCode];
-
+        if(code.length<=0) throw new RuntimeException();
         for (int i = 0; i < code.length; i++) {
             code[i] = random.nextInt(numberOfColors);
         }
