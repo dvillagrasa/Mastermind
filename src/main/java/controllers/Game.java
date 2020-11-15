6b6a2502ae8fdb466331a083aa18e6bddd1234d0
Sergi Luque <sergi1008@gmail.com>
2020-11-15 21:34:00 +0100

    feat: :sparkles: Created controller class Game

---
+++ src/main/java/controllers/Game.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import core.Board;
+
+public class Game {
+
+    private CodeBreaker codeBreaker;
+    private CodeMaker codeMaker;
+    private Board board;
+    private int attempts;
+
+    public Game(CodeMaker codeMaker, CodeBreaker codeBreaker,int attempts) {
+        this.codeMaker = codeMaker;
+        this.codeBreaker = codeBreaker;
+        this.attempts = attempts;
+    }
+
+    public void start() {
+        board = new Board(codeMaker.generateCode(), this.attempts);
+    }
+   
+
+    
+}
